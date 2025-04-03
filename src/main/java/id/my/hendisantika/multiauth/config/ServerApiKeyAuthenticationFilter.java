f411860cc48589d80ea0ad2ff9d33646feb0b63e
hendisantika <hendisantika@yahoo.co.id>
2025-04-04 06:48:26 +0700

    feat: Add ServerApiKeyAuthenticationFilter🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/multiauth/config/ServerApiKeyAuthenticationFilter.java
@@ -0,0 +1,64 @@
+package id.my.hendisantika.multiauth.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-multi-auth
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 04/04/25
+ * Time: 06.47
+ * To change this template use File | Settings | File Templates.
+ */
+
+/**
+ * A custom Spring Security filter responsible for retrieving the HTTP API Key header from the request
+ * and validating it against the configured secret.
+ */
+public class ServerApiKeyAuthenticationFilter extends GenericFilterBean {
+    /**
+     * Extracts the API Key header from the request, validates it against the configured secret,
+     * and sets the resulting Authentication object into the SecurityContext.
+     * If validation fails, an unauthorized status code (401) is sent in the response.
+     *
+     * @param request     the incoming ServletRequest
+     * @param response    the outgoing ServletResponse
+     * @param filterChain the next filter in the chain
+     * @throws IOException      if an I/O error occurs during processing
+     * @throws ServletException if an error occurs that interferes with the filter chain
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+        try {
+            Authentication authentication = AuthenticationService.getAuthentication((HttpServletRequest) request);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (Exception exp) {
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            httpResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            PrintWriter writer = httpResponse.getWriter();
+            writer.print(exp.getMessage());
+            writer.flush();
+            writer.close();
+            return; // Don't continue processing the filter chain if authentication fails
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
