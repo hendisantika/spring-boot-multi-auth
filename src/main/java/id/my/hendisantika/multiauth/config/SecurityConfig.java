b48ca3dc77b462a680eb7cc929b00d54cd8f0520
hendisantika <hendisantika@yahoo.co.id>
2025-04-04 06:52:48 +0700

    feat: Add filterChainServers in SecurityConfig🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/multiauth/config/SecurityConfig.java
+++ src/main/java/id/my/hendisantika/multiauth/config/SecurityConfig.java
@@ -1,7 +1,14 @@
 package id.my.hendisantika.multiauth.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,4 +24,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    @Order(1)
+    public SecurityFilterChain filterChainServers(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/api/multi-auth/server/**")
+                .addFilterBefore(new ServerApiKeyAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
 }
