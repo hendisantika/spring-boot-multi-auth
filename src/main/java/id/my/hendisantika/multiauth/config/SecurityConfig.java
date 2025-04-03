d2461c6f6094d714d04aa09f8a14753c5b07f73c
hendisantika <hendisantika@yahoo.co.id>
2025-04-04 06:53:13 +0700

    feat: Add filterChainClients in SecurityConfig🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/multiauth/config/SecurityConfig.java
+++ src/main/java/id/my/hendisantika/multiauth/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package id.my.hendisantika.multiauth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -25,6 +27,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
+    public AuthenticationProvider authenticationProvider() {
+        return new AuthorizationServerAuthenticationProvider();
+    }
+
+    @Bean
     @Order(1)
     public SecurityFilterChain filterChainServers(HttpSecurity http) throws Exception {
         http
@@ -33,6 +40,20 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain filterChainClients(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorization -> authorization
+                        .requestMatchers("/api/multi-auth/client/**").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 }
