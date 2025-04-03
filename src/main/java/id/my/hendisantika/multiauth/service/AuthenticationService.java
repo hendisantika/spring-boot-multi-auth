0ceb45f53ab68ed4b23e88185a6fe350adea7ae9
hendisantika <hendisantika@yahoo.co.id>
2025-04-04 06:51:04 +0700

    feat: Add AuthenticationService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/multiauth/service/AuthenticationService.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.multiauth.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-multi-auth
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 04/04/25
+ * Time: 06.50
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class AuthenticationService {
+
+    /**
+     * The name of the HTTP header containing the API Key.
+     */
+    public static final String AUTH_TOKEN_HEADER_NAME = "API-KEY";
+
+}
