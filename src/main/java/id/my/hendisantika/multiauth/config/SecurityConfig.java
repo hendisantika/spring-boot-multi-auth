2369b9b52de7afb61b8ee7abf1ebd41be193df30
hendisantika <hendisantika@yahoo.co.id>
2025-04-04 06:52:36 +0700

    feat: Add SecurityConfig🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/multiauth/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.multiauth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-multi-auth
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 04/04/25
+ * Time: 06.52
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+}
