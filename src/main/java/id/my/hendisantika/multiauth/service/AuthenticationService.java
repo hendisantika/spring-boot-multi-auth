d12de8622b1d426ed2ca275795e693d05231b550
hendisantika <hendisantika@yahoo.co.id>
2025-04-04 06:51:20 +0700

    feat: Add getAuthentication in AuthenticationService🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/multiauth/service/AuthenticationService.java
+++ src/main/java/id/my/hendisantika/multiauth/service/AuthenticationService.java
@@ -1,5 +1,10 @@
 package id.my.hendisantika.multiauth.service;
 
+import id.my.hendisantika.multiauth.config.ApiKeyAuthentication;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.stereotype.Service;
 
 /**
@@ -21,4 +26,20 @@ public class AuthenticationService {
      */
     public static final String AUTH_TOKEN_HEADER_NAME = "API-KEY";
 
+    /**
+     * Attempts to retrieve and validate the API Key from the request header.
+     * If valid, it creates an Authentication object representing the API Key.
+     *
+     * @param request the HttpServletRequest object
+     * @return an Authentication object containing the validated API Key, or throws an exception if invalid
+     * @throws BadCredentialsException if the API Key is missing or invalid
+     */
+    public static Authentication getAuthentication(HttpServletRequest request) {
+        String apiKey = request.getHeader(AUTH_TOKEN_HEADER_NAME);
+        if (apiKey == null || !apiKey.equals("12345abc")) {
+            throw new BadCredentialsException("Invalid API Key");
+        }
+
+        return new ApiKeyAuthentication(apiKey, AuthorityUtils.NO_AUTHORITIES);
+    }
 }
