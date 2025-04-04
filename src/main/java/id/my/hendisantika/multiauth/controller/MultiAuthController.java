354df260acec9ac59363e9e8c6245c200604b024
hendisantika <hendisantika@yahoo.co.id>
2025-04-04 07:24:30 +0700

    feat(API): Add logger🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/multiauth/controller/MultiAuthController.java
+++ src/main/java/id/my/hendisantika/multiauth/controller/MultiAuthController.java
@@ -1,5 +1,6 @@
 package id.my.hendisantika.multiauth.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,17 +18,20 @@ import java.security.Principal;
  * Time: 07.15
  * To change this template use File | Settings | File Templates.
  */
+@Slf4j
 @RestController
 @RequestMapping("/api/multi-auth")
 public class MultiAuthController {
 
     @GetMapping("/server/test")
     public String serverTestEndpoint() {
+        log.info("This is a secured endpoint! You have successfully authenticated using an API key.");
         return "This is a secured endpoint! You have successfully authenticated using an API key.";
     }
 
     @GetMapping("/client/test")
     public String clientTestEndpoint(Principal principal) {
+        log.info("This is a secured endpoint for clients! You are authenticated as: {}", principal.getName());
         return "This is a secured endpoint for clients! You are authenticated as: " + principal.getName();
     }
 }
